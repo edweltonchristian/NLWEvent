5edf0c4fee1d775adb83fa34cbe5f74957b3386f
edweltonchristian <105066241+edweltonchristian@users.noreply.github.com>
2022-05-10 13:48:14 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+// string == (textos)
+// Number == (número)
+// Boolean  == (true | false)
+
+function onScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
