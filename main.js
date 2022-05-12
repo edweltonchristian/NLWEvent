0d8c70fa756a347343f51b5f52852087df241a30
edweltonchristian <105066241+edweltonchristian@users.noreply.github.com>
2022-05-11 21:46:47 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,8 +5,45 @@ window.addEventListener('scroll', onScroll)
 onScroll()
 
 function onScroll() {
- showNavOnScroll()
- showBackToTopButtonOnScroll()
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+
+}
+
+function activateMenuAtCurrentSection (section) {
+  // Descobrir a Linha alvo "meio da tela"
+  const targetLine = scrollY + innerHeight / 2
+
+  //Quais dados vou precisar?
+  // Valor do topo da seção
+  const sectionTop = section.offsetTop
+  // Valor do tamanho(Altura) da seção
+  const sectionHeight = section.offsetHeight
+  // Final da seção (valor inicial + o tamanho da seção)
+  const sectionEndsAt = sectionTop + sectionHeight
+  
+  // Verificar se o topo da seção passou da linha alvo
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+  // Verificar se o final da seção passou da linha alvo
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+  // Limites da Seção ------------ ! == negação do boleano
+  const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  // Pegar o valor do atributo da classe
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  // Remove e adiciona a classe remove
+  menuElement.classList.remove('active')
+  if(sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
+
 }
 
 function showNavOnScroll() {
